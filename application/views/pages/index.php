3da5c13c74711af4dea8faeef35731ed0465f704
dominik Borek <dominikborek@seznam.cz>
2020-10-14 13:37:47 +0200

    first commit

---
+++ application/views/pages/index.php
@@ -0,0 +1,32 @@
+		<br>
+		<h1>SEZNAM LITERÁRNÍCH DĚL K MATURITĚ</h1>
+		<img src="assets/pic/book.png" alt="book" class="center">
+		<div class="container">
+
+		<h2>Ukázka knih</h2>
+			<table class="table">				
+		  		<thead>
+				    <tr  class="table-warning">
+				      <th scope="col"></th>
+				      <th scope="col">Název knihy</th>
+				      <th scope="col">Autor</th>
+				    </tr>
+			 	</thaed>
+			 	<tbody>
+			 	<?php if ($knihy): ?>
+                 <?php foreach($knihy as $knihy): ?>
+				    <tr class="table-secondary">
+				      <td><?php echo $knihy->id_knihy;?></td>
+
+				      <td><a href="<?php echo base_url("kniha/".$knihy->id_knihy);?>"><?php echo $knihy->nazev_knihy;?></td>
+
+				      <td><?php echo $knihy->autor;?></td>
+				    </tr>
+			     <?php endforeach ?>
+			    <?php endif ?>
+				</tbody>
+				 </div>
+			</table> 
+		 </div>
+		</div>
+	</body>
---
+++ application/views/pages/prvni_strana.php
@@ -0,0 +1,28 @@
+<div class="container">
+  <br>
+  <h1>světová a česká literatura 18. století</h1>
+<img src="assets/pic/book_1.png" alt="book" class="center_1">
+  <table class="table">   
+  <thead>
+    <tr class="table-warning">
+      <th scope="col">Kniha</th>
+      <th scope="col">Autor</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php if ($knihy): ?>
+     <?php foreach($knihy as $knihy): ?>
+      <tr class="table-secondary">
+            <td>
+              <a href="<?php echo base_url("kniha/".$knihy->id_knihy);?>">
+                <?php echo $knihy->nazev_knihy;?>
+            </td>
+            <td> 
+                <?php echo $knihy->autor; ?>
+            </td>
+    </tr>
+  <?php endforeach; ?>
+  <?php endif; ?>
+  </tbody>
+</table>
+</div>
